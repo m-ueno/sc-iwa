88f22cf359e3b144d1ea2c9b835b5f3164f69ce8
uenop <dummy@mail.com>
2012-01-12 17:46:13 +0900

    allocate

--- jacobi2.f95
+++ jacobi2.f95
@@ -1,23 +1,38 @@
 program jacobi2
   implicit none
-  integer,parameter :: n = 900, max_iter = 100000, nzero=3924
+  integer,parameter :: max_iter = 100000
   double precision,parameter :: epsilon = 1e-7
 
-  integer :: i,iold=0, j, k, jj, iter
-  integer :: col_idx(nzero), row_ptr(n+1), row_ptr_index=1
-  double precision :: res,val
-  double precision :: a(nzero),b(n),x(n),xold(n), r(n), d(n)
+  integer :: i,iold=0, j, k, iter, n, nzero
+  integer :: row_ptr_index=1
+  integer,allocatable,dimension(:) :: row_ptr, col_idx
+  double precision :: res, val, r_norm, b_norm
+  double precision,allocatable,dimension(:) :: a, b, x, xold, r, diag
+
+  ! initialize n, nzero
+  open (10,file='poisson.matrix.900.data')
+  read(10,*) n,nzero                !900, 3924
+
+  allocate(a(nzero))
+  allocate(col_idx(nzero))
+
+  allocate(row_ptr(n+1))
+  allocate(b(n))
+  allocate(x(n))
+  allocate(xold(n))
+  allocate(r(n))
+  allocate(diag(n))
+
+  xold = 0d0
 
   !! CRS
-  open (10,file='poisson.matrix.900.data')
-  read(10,*) i,i                !900, 3924
   do k=1,nzero
      read(10,*) i,j,val
      a(k) = val
      col_idx(k) = j
 
      if ( i == j ) then
-        d(i) = val
+        diag(i) = val
      end if
 
      if (i>iold) then
@@ -27,53 +42,61 @@ program jacobi2
      endif
   enddo
 
-  print *, "row_ptr", row_ptr
-  ! ok �äݤ�
+  ! trick
+  row_ptr(row_ptr_index) = nzero+1
 
   open (20,file='poisson.rhs.900.data.mod')
   do i = 1,n
-     read(20,*) l
-     b(i) = l
+     read(20,*) val
+     b(i) = val
      !   print *, val !! debug
   enddo
 
-  xold = 0d0
+  b_norm = sqrt(dot_product(b,b))
 
   !! start main loop
-  do k=1, max_iter
-
-     res = 0d0                  ! residual
+  do iter=1, max_iter
 
      do i=1,n
 
         x(i) = b(i)
 
-!        do j=1,n
-        do j = row_ptr(i), row_ptr(i+1)-1
-           jj = col_ind(j)
+        ! k : 非ゼロ要素のval中の序列
+        do k = row_ptr(i), row_ptr(i+1)-1
+           j = col_idx(k)
            if ( i .ne. j ) then
-!              x(i) = x(i) - a(j,i)*xold(j)
-              x(i) = x(i) - a()*xold(j)
+              x(i) = x(i) - a(k)*xold(j)
            end if
         end do
-
-!        x(i) = x(i) / a(i,i)
-        x(i) = x(i) / a()    ! ??
-        res = res + abs( x(i) - xold(i) )
+        
+        x(i) = x(i) / diag(i)
 
      end do
 
-     if ( mod(k,100) == 1 ) then
-        print *, "res", res        !debug
+     !! 残差評価
+     ! 残差ベクトル r = b - Ax
+     do i=1,n
+        r(i) = b(i)
+
+        do k = row_ptr(i), row_ptr(i+1)-1
+           j = col_idx(k)
+           r(i) = r(i) - a(k) * x(j)
+        enddo
+     end do
+
+     r_norm = sqrt(dot_product(r,r))
+
+     if ( mod(iter,100) == 1 ) then
+        print *, "iter=",iter, "r", r_norm
      end if
 
-     if( res < epsilon ) then
+     if( r_norm < epsilon*b_norm ) then
         exit ! break do
      end if
 
      xold = x
 
-  end do
+  end do ! iter
 
   !! print
   do i=1,n
@@ -82,6 +105,6 @@ program jacobi2
      end if
   end do
   
-  print *, "k=",k
+  print *, "iter=",iter
 
 end program jacobi2
