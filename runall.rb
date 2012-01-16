1ec5baa809bd994b3bd6411f9df8be3c75b4aefb
uenop <dummy@mail.com>
2012-01-16 23:18:38 +0900

    ruby

--- runall.rb
+++ runall.rb
@@ -1,10 +1,12 @@
-programs = ["jacobi2", "gauss2", "cg"]
+programs = ["jacobi2", "gauss-seidel2", "cg"]
 sizes = ["900", "2500", "3600"]
 
 programs.each do |prog|
-  sizes.each do |size|
-    p [prog, size]
-    p system("./#{prog} #{size}")
-  end
+    sizes.each do |size|
+        p [prog, size]
+        1.times {
+            system("./#{prog}.out #{size}")
+        }
+    end
 end
 
