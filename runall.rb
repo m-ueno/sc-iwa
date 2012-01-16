94c7fed29347822226e33e52c5c6dc8cd984b006
uenop <dummy@mail.com>
2012-01-16 19:44:24 +0900

    add

---
+++ runall.rb
@@ -0,0 +1,10 @@
+programs = ["jacobi2", "gauss2", "cg"]
+sizes = ["900", "2500", "3600"]
+
+programs.each do |prog|
+  sizes.each do |size|
+    p [prog, size]
+    p system("./#{prog} #{size}")
+  end
+end
+
